cb60e95ee52f6de46cab4d2aa2b932925ca474cb
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-02 10:59:32 +0300

    try add custom middleware

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var app = express();
 // static middleware serving files from the public folder
 app.use(express.static('public'));
 
+// custom middleware
+app.use(function(request, response, next) {
+  console.log('Request come in from: ' + request.hostname);
+  console.log('IP: ' + request.ip);
+  console.log('Method: ' + request.method);
+  console.log('originalUrl: ' + request.originalUrl);
+  console.log('path: ' + request.path);
+  next();
+});
+
 app.get('/', function(request, response) {
   response.sendFile('index.html');
 });
