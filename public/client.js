133cf5f536efc43aeafd3d9f7bfb43d86e68b4bc
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-02 11:52:39 +0300

    add client side js code for AJAX req. Add route for that request. Add css and new background

---
+++ public/client.js
@@ -0,0 +1,13 @@
+$(function(){
+  // AJAX request
+  // return blocks JSON format
+  $.get('blocks', appendToList);
+
+  function appendToList(blocks) {
+    var list = [];
+    for(var i in blocks){
+      list.push($('<li>', { text: blocks[i] }));
+    }
+    $('.block-list').append(list);
+  }
+});
--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var app = express();
 
 // static middleware serving files from the public folder
 app.use(express.static('public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(request, response) {
   response.sendFile('index.html');
 });
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000...');
+app.get('/blocks', function(request, response) {
+  var blocks = ['Fixed', 'Movable', 'Rotating'];
+  response.json(blocks);
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port ' + port + '...');
 });
