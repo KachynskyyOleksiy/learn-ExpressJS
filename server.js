fd6f28e38eebd1a3db054563510d8e7d1f80ce1f
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-02 11:24:12 +0300

    Add port env variable

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.get('/', function(request, response) {
   response.send("Hello world");
 });
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000...');
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on port ' + port + '...');
 });
