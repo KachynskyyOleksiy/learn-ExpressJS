6bd95bbc68fda38eb75e439df843163ae31f8a90
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-02 09:56:35 +0300

    Express Hello World

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello from server.js');
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+  response.send("Hello world");
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000...');
+});
