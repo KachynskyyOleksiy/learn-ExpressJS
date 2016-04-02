a037b619f2b57ee573668081a476debfaa30c988
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-02 11:52:39 +0300

    add client side js code for AJAX req. Add route for that request. Add css and new background
    Conflicts:
    	server.js

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ var app = express();
 
 // static middleware serving files from the public folder
 app.use(express.static('public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.get('/', function(request, response) {
   response.sendFile('index.html');
 });
+
+app.get('/blocks', function(request, response) {
+  var blocks = ['Fixed', 'Movable', 'Rotating'];
+  response.json(blocks);
+});
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
