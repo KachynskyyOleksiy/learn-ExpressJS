19401331ac04db31d4677470a5b07394534aa8b4
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-02 10:08:27 +0300

    Responding with JSON

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  //response.send("Hello world");
-  //We can write the same using node API:
-  response.write('Hello world');
-  response.end();
+  response.send("Hello world");
 });
+
+app.get('/blocks', function(request, response) {
+  var blocks = ['Fixed', 'Movable', 'Rotating'];
+  response.send(blocks);
+});
+
+// send json object.
+// run in terminal:
+// curl -i localhost:3000/blocks
+
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...');
