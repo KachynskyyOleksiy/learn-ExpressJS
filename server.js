25f1491925be0d7aa70858658a7c6f8f77a5bc13
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-03 11:06:44 +0300

    Route Files

--- server.js
+++ server.js
@@ -1,60 +1,16 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({extended: false});
-
-// static middleware serving files from the public folder
+// static middlewares serving files from the public and bower folder
 app.use(express.static('public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-
-var blocks = {
-  'Fixed': 'Fixed description', 
-  'Movable': 'Movable description', 
-  'Rotating': 'Rotating description'
-};
-
-app.param('name', function(request, response, next) {
-  var name = request.params.name;
-  var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
-  
-  request.blockName = block;
-
-  next();
-});
 
 app.get('/', function(request, response) {
   response.sendFile('index.html');
 });
 
-app.route('/blocks')
-  .get(function(request, response) {
-    if (request.query.limit >= 0){
-      response.json(blocks.slice(0, request.query.limit));
-    } else {
-      response.json(Object.keys(blocks));
-    };
-  })
-  .post(parseUrlencoded, function(request, response) {
-    var newBlock = request.body;
-    blocks[newBlock.name] = newBlock.description;
-
-    response.status(201).json(newBlock.name);
-  });
-
-app.route('/blocks/:name')
-  .get(function(request, response) {
-    var description = blocks[request.blockName];
-    if (!description){
-      response.status(404).json('No description found for ' + request.params.name);
-    } else {
-      response.json(description);
-    };
-  })
-  .delete(function(request, response) {
-    delete blocks[request.blockName];
-    response.sendStatus(200);
-  });
+var blocks = require('./routes/blocks');
+app.use('/blocks', blocks);
 
 
 var port = process.env.PORT || 3000;
