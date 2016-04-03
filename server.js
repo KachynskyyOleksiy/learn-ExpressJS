59d65306332e870f22f530f9b776994fb79b2d3d
Oleksiy <k.oleksiy.v@gmail.com>
2016-04-03 08:31:24 +0300

    User Params. Massaging user data

--- server.js
+++ server.js
@@ -5,26 +5,36 @@ var app = express();
 app.use(express.static('public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
-app.get('/', function(request, response) {
-  response.sendFile('index.html');
-});
-
 var blocks = {
   'Fixed': 'Fixed description', 
   'Movable': 'Movable description', 
   'Rotating': 'Rotating description'
 };
 
+
+app.param('name', function(request, response, next) {
+  var name = request.params.name;
+  var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  
+  request.blockName = block;
+
+  next();
+});
+
+app.get('/', function(request, response) {
+  response.sendFile('index.html');
+});
+
 app.get('/blocks', function(request, response) {
   if (request.query.limit >= 0){
     response.json(blocks.slice(0, request.query.limit));
   } else {
-    response.json(blocks);
+    response.json(Object.keys(blocks));
   };
 });
 
 app.get('/blocks/:name', function(request, response) {
-  var description = blocks[request.params.name];
+  var description = blocks[request.blockName];
   if (!description){
     response.status(404).json('No description found for ' + request.params.name);
   } else {
@@ -34,7 +44,8 @@ app.get('/blocks/:name', function(request, response) {
 
 // test:
 // curl -i localhost:3000/blocks/Fixed
-// curl -i localhost:3000/blocks/Banana
+// curl -i localhost:3000/blocks/fixed
+// curl -i localhost:3000/blocks/FiXeD
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
